6dc4632816b1d621b1cc35b85e10da9f53cd00d8
terryjung0244 <terryjung0244@gmail.com>
2023-07-11 22:21:20 -0400

    ShowTodo Updated

--- src/service/components/common/Input/Input.tsx
+++ src/service/components/common/Input/Input.tsx
@@ -4,7 +4,12 @@ import { InputPropsType } from './Input.interface';
 const Input = ({ name, value, placeholder, onChange }: InputPropsType) => {
   return (
     <div>
-      <input name={name} value={value} placeholder={placeholder} onChange={onChange} />
+      <input
+        name={name}
+        value={value}
+        placeholder={placeholder}
+        onChange={onChange}
+      />
     </div>
   );
 };
--- src/service/components/showTodo/ShowTodo.tsx
+++ src/service/components/showTodo/ShowTodo.tsx
@@ -1,15 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ShowTodo.css';
 import { TodoType } from 'service/model/todo';
 import { useAppSelector } from 'service/store';
+import { AiFillEdit } from 'react-icons/ai';
+import { IoTrashOutline } from 'react-icons/io5';
+import Input from '../common/Input/Input';
+import { UpdateInputStateType } from './ShowTodo.interface';
 
 const ShowTodo = () => {
+  const [showUpdateInput, setShowUpdateInput] = useState<boolean>(false);
+  const [updateInput, setUpdateInput] = useState<UpdateInputStateType>({
+    task: '',
+  });
   const { todoList } = useAppSelector((state) => state.todoReducer);
+
+  const updateTodo = (id: string) => {
+    console.log(id);
+    setShowUpdateInput(true);
+  };
+
+  const updateTaskInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUpdateInput({ ...updateInput, [e.target.name]: e.target.value });
+  };
+
+  const updateNewTask = () => {
+    // validation
+    const { task } = updateInput;
+    console.log(task);
+  };
 
   console.log(todoList);
 
   return (
     <div>
+      <div>
+        {showUpdateInput && (
+          <div>
+            <Input
+              name="task"
+              value={updateInput.task}
+              placeholder="Update"
+              onChange={updateTaskInput}
+            />
+            <button onClick={updateNewTask}>Update</button>
+          </div>
+        )}
+      </div>
       <table>
         <thead>
           <tr>
@@ -27,8 +63,14 @@ const ShowTodo = () => {
                 <td>{todo.id}</td>
                 <td>{todo.task}</td>
                 <td>{todo.status}</td>
-                <td>Progressing</td>
-                <td>Completed</td>
+                <td onClick={() => updateTodo(todo.id)}>
+                  Update
+                  <AiFillEdit />
+                </td>
+                <td>
+                  Delete
+                  <IoTrashOutline />
+                </td>
               </tr>
             );
           })}
